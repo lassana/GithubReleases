5e5491fc28015e5f5d13b60c7cfd0df117210c5b
lassana <lassana.nd@gmail.com>
2014-01-10 07:44:43 +0000

    Add project.

---
+++ app/src/main/java/com/github/lassana/releases/fragment/MainFragment.java
@@ -0,0 +1,25 @@
+package com.github.lassana.releases.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.lassana.releases.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
---
+++ github/src/main/java/com/github/lassana/releases/github/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author lassana
+ * @since 1/10/14
+ */
+package com.github.lassana.releases.github;
