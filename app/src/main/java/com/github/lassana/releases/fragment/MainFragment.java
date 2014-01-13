ebf6b63fd11beb7c6ff2a6e0841840bd7c5238ee
lassana <lassana.nd@gmail.com>
2014-01-13 07:41:36 +0000

    Add listview with items from 'repositories' table.

--- app/src/main/java/com/github/lassana/releases/fragment/MainFragment.java
+++ app/src/main/java/com/github/lassana/releases/fragment/MainFragment.java
@@ -1,17 +1,28 @@
 package com.github.lassana.releases.fragment;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListAdapter;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.github.lassana.releases.R;
 import com.github.lassana.releases.github.api.Repository;
 import com.github.lassana.releases.github.model.Tag;
+import com.github.lassana.releases.storage.model.RepositoriesContract;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -21,16 +32,48 @@ import java.util.List;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends ListFragment {
 
     private static final String TAG = "MainFragment";
+
+    private static final int LOADER_ID = 1;
+    private static final String[] PROJECTION = {
+            RepositoriesContract.Repositories._ID,
+            RepositoriesContract.Repositories.USER_NAME,
+            RepositoriesContract.Repositories.REPOSITORY_NAME};
+
+    private CursorAdapter mAdapter;
+
+    private LoaderManager.LoaderCallbacks<Cursor> loaderCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
+        @Override
+        public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+            return new CursorLoader(
+                    getActivity(),
+                    RepositoriesContract.Repositories.CONTENT_URI,
+                    PROJECTION,
+                    null,
+                    null,
+                    null
+            );
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+            mAdapter.swapCursor(cursor);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> cursorLoader) {
+            mAdapter.swapCursor(null);
+        }
+    };
 
     public MainFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
@@ -40,10 +83,23 @@ public class MainFragment extends Fragment {
         view.findViewById(android.R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ContentValues repository = new ContentValues();
+                repository.put(RepositoriesContract.Repositories.USER_NAME, "lassana");
+                repository.put(RepositoriesContract.Repositories.REPOSITORY_NAME, "listview-anim-sorting");
+                Uri uri = getActivity().getApplicationContext().getContentResolver().insert(RepositoriesContract.Repositories.CONTENT_URI, repository);
+                Log.d(TAG, uri.toString());
             }
         });
+        mAdapter = new SimpleCursorAdapter(
+                getActivity(),
+                android.R.layout.simple_list_item_2,
+                null,
+                new String[]{RepositoriesContract.Repositories.USER_NAME, RepositoriesContract.Repositories.REPOSITORY_NAME},
+                new int[]{android.R.id.text1, android.R.id.text2},
+                0);
+        getListView().setAdapter(mAdapter);
+        getActivity().getSupportLoaderManager().initLoader(LOADER_ID, null, loaderCallbacks);
     }
-
 
 
 }
